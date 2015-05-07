31ef9db149b27d047de1765e2a6e5e1a792b86f1
Luiz Picolo <luizpicolo@gmail.com>
2015-05-06 23:48:42 -0400

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,54 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-# for rvm support. (http://rvm.io)
 require 'mina/rvm'
 
 # Repository project
-set :repository, ''
+set :repository, 'git@bitbucket.org:agencia-w3case/boletinonline.git'
 
 # Server Production
 task :production do
   set :rails_env, 'production'
-  set :user, ''
-  set :domain, ''
-  set :deploy_to, '/home/user/public_html/railsapp'
+  set :user, 'boletimonlineanaec'
+  set :domain, 'app.w3case.com.br'
+  set :deploy_to, '/home/boletimonlineanaec/public_html/railsapp'
   set :branch, 'deploy'
 end
 
 # Server Staging
 task :staging do
   set :rails_env, 'staging'
-  set :user, ''
-  set :domain, ''
-  set :deploy_to, '/home/user/public_html/railsapp'
+  set :user, 'boletinonline_anaec'
+  set :domain, '192.168.1.150'
+  set :deploy_to, '/home/boletinonline_anaec/public_html/railsapp'
   set :branch, 'master'
 end
+
+# Server development
+# task :development do
+#   set :rails_env, 'development'
+#   set :user, ''
+#   set :domain, ''
+#   set :deploy_to, ''
+#   set :branch, 'development'
+# end
 
 # Fix
 set :term_mode, nil
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log', 'config/application.yml', 'config/secrets.yml']
+set :shared_paths, ['config/database.yml', 'log', 'tmp', 'config/application.yml']
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
 task :environment do
-  invoke :'rvm:use[ruby-2.2.1@gemset]'
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .rbenv-version to your repository.
+  # invoke :'rbenv:load'
+
+  # For those using RVM, use this to load an RVM version@gemset.
+  invoke :'rvm:use[ruby-2.1.3@boilerplate]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
@@ -48,31 +61,29 @@ task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/storage"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/storage"]
 
-  # Env deploy
   queue! %[touch "#{deploy_to}/storage/index.html"]
 
   queue! %[mkdir -p "#{deploy_to}/shared/config"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/pids"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/pids"]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/tmp"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/tmp"]
 
   queue! %[touch "#{deploy_to}/shared/config/database.yml"]
   queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
 
   queue! %[touch "#{deploy_to}/shared/config/application.yml"]
   queue  %[echo "-----> Be sure to edit 'shared/config/application.yml'."]
-  
-  # Create secrets_token
-  queue! %[touch "#{deploy_to}/shared/config/secrets.yml"]
-  queue  %[echo "-----> Be sure to edit 'shared/config/secrets.yml'."]
-
-  # sidekiq needs a place to store its pid file and log file
-  queue! %[mkdir -p "#{deploy_to}/shared/pids/"]
-  queue! %[mkdir -p "#{deploy_to}/shared/log/"]
 end
 
+# Show logs
 desc "Show logs rails."
 task :'logs:rails' => :environment do
   queue 'echo "Contents of the log file are as follows:"'
-  queue "tail -f #{deploy_to}/current/log/#{rails_env}.log"
+  queue "tail -f #{deploy_to}/shared/log/production.log"
 end
 
 desc "Show logs Nginx."
@@ -92,10 +103,7 @@ task :deploy => :environment do
 
     to :launch do
       queue %[echo -n "-----> Creating new restart.txt: "]
-      queue "touch #{deploy_to}/tmp/restart.txt"
-
-      queue %[echo -n "-----> Restart nginx Service: "]
-      queue 'sudo service nginx restart'
+      queue "touch #{deploy_to}/shared/tmp/restart.txt"
     end
   end
 end
@@ -115,8 +123,8 @@ task :rollback => :environment do
   queue %[ls "#{deploy_to}/releases" -Art | sort | tail -n 1]
   queue! %[ls "#{deploy_to}/releases" -Art | sort | tail -n 1 | xargs -I active rm -rf "#{deploy_to}/releases/active"]
 
-  #queue %[echo -n "-----> Restart Apache Service: "]
-  queue 'sudo service nginx restart'
+  queue %[echo -n "-----> Creating new restart.txt: "]
+  queue "touch #{deploy_to}/shared/tmp/restart.txt"
 end
 
 # Maintenance
@@ -136,6 +144,7 @@ task :'system:turnon' => :environment do
 end
 
 # Server
+# Need sudo user
 # Restart Nginx
 desc "Restart Server"
 task :'server:restart' => :environment do
