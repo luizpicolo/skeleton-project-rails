82a24833695088fce0e89fc5200904c61e72876f
Luiz Picolo <luizpicolo@gmail.com>
2015-11-25 15:02:39 -0300

    update deploy.rb and ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-# for rvm support. (http://rvm.io)
 require 'mina/rvm'
+
+set :ruby_version, '2.2.1'
 
 # Repository project
 set :repository, ''
@@ -12,8 +13,8 @@ task :production do
   set :rails_env, 'production'
   set :user, ''
   set :domain, ''
-  set :deploy_to, '/home/user/public_html/railsapp'
-  set :branch, 'deploy'
+  set :deploy_to, ''
+  set :branch, 'master'
 end
 
 # Server Staging
@@ -21,8 +22,17 @@ task :staging do
   set :rails_env, 'staging'
   set :user, ''
   set :domain, ''
-  set :deploy_to, '/home/user/public_html/railsapp'
+  set :deploy_to, ''
   set :branch, 'master'
+end
+
+# Server development
+task :development do
+  set :rails_env, 'development'
+  set :user, ''
+  set :domain, ''
+  set :deploy_to, ''
+  set :branch, ''
 end
 
 # Fix
@@ -30,12 +40,17 @@ set :term_mode, nil
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log', 'config/application.yml', 'config/secrets.yml']
+set :shared_paths, ['config/database.yml', 'log', 'tmp', 'config/application.yml', 'config/secrets.yml']
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
 task :environment do
-  invoke :'rvm:use[ruby-2.2.1@gemset]'
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .rbenv-version to your repository.
+  # invoke :'rbenv:load'
+
+  # For those using RVM, use this to load an RVM version@gemset.
+  invoke :"rvm:use[ruby-#{ruby_version}@#{user}]"
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
@@ -48,11 +63,16 @@ task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/storage"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/storage"]
 
-  # Env deploy
   queue! %[touch "#{deploy_to}/storage/index.html"]
 
   queue! %[mkdir -p "#{deploy_to}/shared/config"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/pids"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/pids"]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/tmp"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/tmp"]
 
   queue! %[touch "#{deploy_to}/shared/config/database.yml"]
   queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
@@ -60,21 +80,8 @@ task :setup => :environment do
   queue! %[touch "#{deploy_to}/shared/config/application.yml"]
   queue  %[echo "-----> Be sure to edit 'shared/config/application.yml'."]
 
-  # sidekiq needs a place to store its pid file and log file
-  queue! %[mkdir -p "#{deploy_to}/shared/pids/"]
-  queue! %[mkdir -p "#{deploy_to}/shared/log/"]
-end
-
-desc "Show logs rails."
-task :'logs:rails' => :environment do
-  queue 'echo "Contents of the log file are as follows:"'
-  queue "tail -f #{deploy_to}/current/log/#{rails_env}.log"
-end
-
-desc "Show logs Nginx."
-task :'logs:nginx' => :environment do
-  queue 'echo "Contents of the log file are as follows:"'
-  queue "tail -f /opt/nginx/logs/error.log"
+  queue! %[touch "#{deploy_to}/shared/config/secrets.yml"]
+  queue  %[echo "-----> Be sure to edit 'shared/config/secrets.yml'."]
 end
 
 desc "Deploys the current version to the server."
@@ -88,10 +95,7 @@ task :deploy => :environment do
 
     to :launch do
       queue %[echo -n "-----> Creating new restart.txt: "]
-      queue "touch #{deploy_to}/tmp/restart.txt"
-
-      queue %[echo -n "-----> Restart nginx Service: "]
-      queue 'sudo service nginx restart'
+      queue "touch #{deploy_to}/shared/tmp/restart.txt"
     end
   end
 end
@@ -111,8 +115,8 @@ task :rollback => :environment do
   queue %[ls "#{deploy_to}/releases" -Art | sort | tail -n 1]
   queue! %[ls "#{deploy_to}/releases" -Art | sort | tail -n 1 | xargs -I active rm -rf "#{deploy_to}/releases/active"]
 
-  #queue %[echo -n "-----> Restart Apache Service: "]
-  queue 'sudo service nginx restart'
+  queue %[echo -n "-----> Creating new restart.txt: "]
+  queue "touch #{deploy_to}/shared/tmp/restart.txt"
 end
 
 # Maintenance
@@ -129,48 +133,4 @@ task :'system:turnon' => :environment do
   queue %[echo -n "-----> Turn Off System: "]
   queue! %[cd "#{deploy_to}/current"]
   queue "RAILS_ENV=#{rails_env} bundle exec rake maintenance:end"
-end
-
-# Server
-# Restart Nginx
-desc "Restart Server"
-task :'server:restart' => :environment do
-  queue %[echo -n "-----> Restart nginx Service: "]
-  queue 'sudo service nginx restart'
-end
-
-# Start Nginx
-desc "Start Server"
-task :'server:start' => :environment do
-  queue %[echo -n "-----> Start nginx Service: "]
-  queue 'sudo service nginx start'
-end
-
-# Stop Nginx
-desc "Stop Server"
-task :'server:stop' => :environment do
-  queue %[echo -n "-----> Stop nginx Service: "]
-  queue 'sudo service nginx stop'
-end
-
-# Migration Seed
-desc "Seed"
-task :'db:seed' => :environment do
-  queue %[echo -n "-----> Seed: "]
-  queue! %[cd "#{deploy_to}/current"]
-  queue "RAILS_ENV=#{rails_env} bundle exec rake db:seed"
-end
-
-desc "Create DB"
-task :'db:create' => :environment do
-  queue %[echo -n "-----> Seed: "]
-  queue! %[cd "#{deploy_to}/current"]
-  queue "RAILS_ENV=#{rails_env} bundle exec rake db:create"
-end
-
-desc "Reset DB"
-task :'db:reset' => :environment do
-  queue %[echo -n "-----> Seed: "]
-  queue! %[cd "#{deploy_to}/current"]
-  queue "RAILS_ENV=#{rails_env} bundle exec rake db:migrate:reset"
 end
